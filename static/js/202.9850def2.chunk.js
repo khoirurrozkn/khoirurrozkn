d20bd826cb9ed850ece5018857161143daf6432a
khoirurrozkn <khoirurrozkn@gmail.com>
2023-10-03 05:21:00 +0700

    Updates

--- static/js/202.9850def2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkkhoirurrozkn=self.webpackChunkkhoirurrozkn||[]).push([[202],{202:function(r,e,n){n.r(e),n.d(e,{default:function(){return d}});n(791);var s=n(184),d=function(){return(0,s.jsxs)("div",{className:"header vh-100 border border-danger d-flex justify-content-evenly align-items-center",children:[(0,s.jsx)("div",{className:"border border-black"}),(0,s.jsx)("div",{className:"border border-danger"})]})}}}]);
-//# sourceMappingURL=202.9850def2.chunk.js.map
